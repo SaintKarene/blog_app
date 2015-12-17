c5e2757a6f06f6d03e8439b7508665e29b75fd19
stkarene <stkarene@gmail.com>
2015-12-17 18:29:19 +0000

    force signin or signup before posting comments

--- blog_app/app/controllers/comments_controller.rb
+++ blog_app/app/controllers/comments_controller.rb
@@ -1,9 +1,12 @@
 class CommentsController < ApplicationController
  before_action :set_article
- 
-    
- def create
-    @comment = @article.comments.build(comment_params)
+
+    def create
+     unless current_user
+    flash[:danger]= "Please sign in or sign up first."
+    redirect_to new_user_session_path
+    else
+     @comment = @article.comments.build(comment_params)
     @comment.user = current_user
     
     if @comment.save
@@ -12,7 +15,7 @@ class CommentsController < ApplicationController
         flash.now[:danger] = "comment has not been created"
     end
     redirect_to article_path(@article)
- end   
+    end   
     
     private
     
@@ -24,4 +27,5 @@ class CommentsController < ApplicationController
       @article = Article.find(params[:article_id])  
     end
     
+    end
 end
